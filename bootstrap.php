30a8ce7121650a20238317afecfa594eb247d9d3
baileicheng <xbfighting@hotmail.com>
2018-03-30 18:10:34 +0800

    [mod] load env

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,11 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Recca0120\LaravelTracy\Tracy;
+use Dotdenv\Dotdenv;
+use Dotenv\Dotenv;
 
 Tracy::instance();
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
 
 ?>
